94697308909ecf3d00d3c59e1331c463696924b8
Braden MacBeth <bjm1016@sru.edu>
2018-10-22 14:25:05 -0400

    update

--- platforms/browser/www/js/changepages.js
+++ platforms/browser/www/js/changepages.js
@@ -1,12 +1,15 @@
 	
-for(i = 1; i <= 10; i++)
-{
-	$(document).ready(function(){
-			$("#book"+i+"content").load("../pages/book"+i+".html");
-	})
-}
+//for(i = 1; i <= 10; i++)
+//{
+	//$(document).ready(function(){
+			//$("#book"+i+"content").load("../pages/book"+i+".html");
+		//("#book" + i + "content").load("../pages/test.html");
+	//})
+//}
 
 function changepage(pageNumber)
 {
-	$change("#book"+pageNumber);
+	document.write("here");
+	$("book" + pageNumber).load("../test.html book" + pageNumber);
+	//$change("#book"+pageNumber);
 }
